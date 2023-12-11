23ba0eb56810c00b4c64b7ff8838b6e26f72ce90
cowanweks <cowannwekesa@gmail.com>
2023-12-11 14:12:20 +0300

    Updated

--- setup.py
+++ setup.py
@@ -1,30 +1,19 @@
 import os
 import logging
 
-def enum_folders_and_create_readmes(path: str) -> list:
+def enum_folders_and_create_readmes() -> list:
+    path = os.getcwd()
+
     # A file pattern to ignore
     ignore_pattern = [
         ".git", ".gitignore", "README.md"
     ]
 
-    # Folders in the current directory
-    folders: list[str] = os.listdir(path=path)
-
-    for folder in folders:
-        if folder.__contains__("*.*"):
-            folders.remove(folder)
-
-    print(folders)
-
     # Loop in the current directory
-    for folder in folders:
-        with open(f"{folder}/README.md", "w") as f:
+    for subdir, dirs, files in os.walk(path):
+        with open(f"{subdir}/README.md", "w") as f:
             pass
 
 
 if __name__ == '__main__':
-    # enum_folders_and_create_readmes(".")
-    d = {'clientip': '192.168.0.1', 'user': os.name}
-    FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
-    logging.basicConfig(format=FORMAT, filename="demo.log")
-    logging.error("Error", extra=d)
+    enum_folders_and_create_readmes()
